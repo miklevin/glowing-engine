e99868064152333a0c445915d76cf025acd50c8e
Pipulate <pipulate@gmail.com>
2023-08-28 17:26:08 -0400

    Returned service to timestamping default

--- yourservice.py
+++ yourservice.py
@@ -3,8 +3,8 @@ from time import sleep
 import subprocess
 
 
-# seconds = 30
-seconds = 60 * 5  # 5 minutes
+seconds = 30
+#seconds = 60 * 5  # 5 minutes
 #seconds = 60 * 60 * 24  # 60 seconds x 60 minutes x 24 hours = 1-day
 
 current_directory = "/home/ubuntu/repos/intro/service/"
@@ -14,7 +14,7 @@ while True:
     
     # Un-edit the below lines and change number of seconds above.
     
-    command = ["/home/ubuntu/pyenv/bin/python", "chatshot.py"]  # Replace with your command.
-    subprocess.run(command, cwd=current_directory, check=True)  # Runs command and returns to parent script.
+    #command = ["/home/ubuntu/pyenv/bin/python", "chatshot.py"]  # Replace with your command.
+    #subprocess.run(command, cwd=current_directory, check=True)  # Runs command and returns to parent script.
     
     sleep(seconds)
